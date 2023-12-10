c6b45d9a25c950835bae14d9c1d9e1ead5fa7042
Matt <77593565+0x10variacie@users.noreply.github.com>
2023-12-10 05:16:18 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 console.log("js launched");
-const d = new Date();
-document.getElementById("HourD").innerHTML = d.getHours();
-document.getElementById("MinuteD").innerHTML = d.getMinutes();
-
+function timecycle() {
+    const today = new Date();
+    function timeformat(i){
+        if (i < 10) { i = "0" + i};
+        return i;
+    }
+    hourvalue = today.getHours();
+    minutevalue = today.getMinutes();
+    secondvalue = today.getSeconds();
+    hourvalue = timeformat(hourvalue);
+    minutevalue = timeformat(minutevalue);
+    secondvalue = timeformat(secondvalue);
+    document.getElementById("HourD").innerHTML = hourvalue;
+    document.getElementById("MinuteD").innerHTML = minutevalue;
+    document.getElementById("SecondD").innerHTML = secondvalue;
+}
+setInterval(timecycle, 1000);
